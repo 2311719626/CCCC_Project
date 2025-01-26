350f01a2519a1a6e1f1a0a8e83aa382a5c3e90c8
wendisx <wendisx@DESKTOP-B7LPAMN.>
2025-01-26 14:38:22 +0800

    feat: 新增日志功能

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,12 +6,13 @@
 const { app } = require("./app.js");
 const { mongodbConnection } = require("./config/db.config.js");
 const { redisConnection } = require("./config/redis.config.js");
-
+const {logger} = require('./utils/log.util.js');
+ 
 /* 启动项目，监听3000端口 */
 app.listen(process.env.PORT,process.env.IP,()=>{
     // 连接数据库
     mongodbConnection()
     // 连接redis
     //redisConnection()
-    console.log("Server start at http://" + process.env.IP + ":" + process.env.PORT)
+    logger.info("Server start at http://"+process.env.IP+":"+process.env.PORT)
 })
