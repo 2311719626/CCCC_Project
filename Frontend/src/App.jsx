e3acca3cedac7a9652801751133e0beb2cf92bd5
yaoguowei1 <163629174+yaoguowei1@users.noreply.github.com>
2025-02-07 14:46:23 +0800

    新的src

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import NavigationBar from "./components/NavigationBar";
-import MainContent from "./components/MainContent";
+import { Route } from "react-router-dom";
+import NavigationBar from "./components/start/NavigationBar";
+import MainContent from "./components/start/MainContent";
+import wiki from "./components/wiki/wiki.jsx"
+// import "./components/wiki/wiki.css"
+import  start from "./components/start/start.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <NavigationBar />
-      <MainContent />
-    </div>
+  return (<div>
+    <NavigationBar />
+   <Route path='/start' component={start}/>
+   <Route path='/wiki' component={wiki}/>
+      
+  </div>
   );
 }
 
